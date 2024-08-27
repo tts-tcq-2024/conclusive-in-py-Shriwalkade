a5a55708beba4e8bca414bff29c1c8f2d4452856
Shriwalkade <shrivallabhwalkade@gmail.com>
2024-08-27 11:01:36 +0530

    Update typewise-alert.py

--- typewise-alert.py
+++ typewise-alert.py
@@ -1,47 +1,53 @@
+import unittest
+from typewise_alert import typewise_alert
 
+class TypewiseTest(unittest.TestCase):
+    def test_infers_breach_as_per_limits(self):
+        self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
 
-def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
-  if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
+    def test_infer_breach_too_high(self):
+        self.assertEqual(typewise_alert.infer_breach(120, 50, 100), 'TOO_HIGH')
 
+    def test_infer_breach_normal(self):
+        self.assertEqual(typewise_alert.infer_breach(70, 50, 100), 'NORMAL')
 
-def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
-  return infer_breach(temperatureInC, lowerLimit, upperLimit)
+    def test_passive_cooling_breach(self):
+        cooling = typewise_alert.PassiveCooling()
+        self.assertEqual(cooling.classify_breach(40), 'TOO_HIGH')
+        self.assertEqual(cooling.classify_breach(30), 'NORMAL')
 
+    def test_hi_active_cooling_breach(self):
+        cooling = typewise_alert.HiActiveCooling()
+        self.assertEqual(cooling.classify_breach(50), 'TOO_HIGH')
+        self.assertEqual(cooling.classify_breach(40), 'NORMAL')
 
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
+    def test_med_active_cooling_breach(self):
+        cooling = typewise_alert.MedActiveCooling()
+        self.assertEqual(cooling.classify_breach(45), 'TOO_HIGH')
+        self.assertEqual(cooling.classify_breach(35), 'NORMAL')
 
+    def test_controller_alert(self):
+        alert = typewise_alert.ControllerAlert()
+        self.assertEqual(alert.send_alert('TOO_HIGH'), 'Controller Alert: TOO_HIGH')
 
-def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
+    def test_email_alert(self):
+        alert = typewise_alert.EmailAlert()
+        self.assertEqual(alert.send_alert('TOO_HIGH'), 'To: a.b@c.com\nHi, the temperature is too high')
+        self.assertEqual(alert.send_alert('TOO_LOW'), 'To: a.b@c.com\nHi, the temperature is too low')
+        self.assertEqual(alert.send_alert('NORMAL'), 'No action needed')
 
+    def test_check_and_alert(self):
+        # Test with HiActiveCooling and ControllerAlert
+        result = typewise_alert.check_and_alert(typewise_alert.ControllerAlert(), typewise_alert.HiActiveCooling(), 50)
+        self.assertEqual(result, 'Controller Alert: TOO_HIGH')
 
-def send_to_email(breachType):
-  recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+        # Test with PassiveCooling and EmailAlert
+        result = typewise_alert.check_and_alert(typewise_alert.EmailAlert(), typewise_alert.PassiveCooling(), 30)
+        self.assertEqual(result, 'No action needed')
+
+        # Test with MedActiveCooling and EmailAlert
+        result = typewise_alert.check_and_alert(typewise_alert.EmailAlert(), typewise_alert.MedActiveCooling(), 45)
+        self.assertEqual(result, 'To: a.b@c.com\nHi, the temperature is too high')
+
+if __name__ == '__main__':
+    unittest.main()
